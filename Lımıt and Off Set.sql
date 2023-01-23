c12edafb6074f1b122bd28a1382928b24b2ee879
çağrı dönmez <114502416+ertgrldnmz@users.noreply.github.com>
2023-01-23 12:22:14 +0300

    Add files via upload

---
+++ Lımıt and Off Set.sql
@@ -0,0 +1,21 @@
+--İlk Sorgu
+SELECT * FROM film
+WHERE title ILIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+--ikinci Sorgu
+SELECT * FROM film
+WHERE title ILIKE '%n'
+ORDER BY length ASC
+OFFSET 5
+LIMIT 5;
+
+--ÜÇÜNCÜ SORGU
+SELECT * FROM customer
+WHERE store_id=1
+ORDER BY last_name DESC
+LIMIT 4;
+
+
+
